f15f541a54f85ddaa7fe698a082bce61608202a1
marco <marco@DESKTOP-H6IVC0U>
2024-06-04 19:12:55 -0600

    Practica 2 inicial

---
+++ src/test/java/com/gamerStore/GamerStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamerStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamerStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
